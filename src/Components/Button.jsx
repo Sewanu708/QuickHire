43d9f2f01ddd8957491e61da2a64d4e5a0095d71
Sewanu Isaiah <sewanu708@gmail.com>
2025-06-13 23:41:20 +0100

    Implemented Nav-bar

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({ content, }) {
     return (
-        <button className='gradient-bg px-4 rounded-[4px] py-2 text-white font-inter transition duration-300 cursor-pointer hover:scale-105'>
+        <button className='w-fit gradient-bg px-4 rounded-[4px] py-2 text-white font-inter transition duration-300 cursor-pointer hover:scale-105'>
             {content}
         </button>
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router'
 import { RiMenu3Fill } from "react-icons/ri";
 import Button from './Button'
 import { IoMdClose } from "react-icons/io";
-import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence, easeInOut, motion } from 'framer-motion';
 function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false)
     return (
@@ -29,31 +29,33 @@ function Navbar() {
                 {<Button content={'Get Started Free'} />}
             </div>
 
-            <div className='flex xl:hidden  relative sm:text-[30px] text-[20px] cursor-pointer ' onClick={() => setMenuOpen(prev => !prev)}>
-                {menuOpen ?  <IoMdClose />:<RiMenu3Fill /> 
+            <div className='flex xl:hidden  sm:text-[40px] text-[30px] cursor-pointer ' onClick={() => setMenuOpen(prev => !prev)}>
+                {menuOpen ? <IoMdClose /> : <RiMenu3Fill />
                 }
             </div>
 
-            {
-                <AnimatePresence>
-                    menuOpen && (
-                    <motion.div className='absolute'>
+
+            <AnimatePresence>
+                {menuOpen && (
+                    <motion.div initial={{
+                        scale: 0, opacity: 0
+                    }} animate={{ scale: 1, opacity: 1 }} exit={{ scale: 0, opacity: 0 }} transition={{ duration: 0.3, ease: easeInOut }} className='absolute top-16 right-0 backdrop-blur-2xl w-[200px] flex flex-col gap-y-2'>
                         {
                             services.map((service, index) => (
-                                <div key={index} className='font-inter capitalize font-[500] cursor-pointer text-black hover:text-[#6c6c71] hover:scale-105 transition-all duration-300 ease-in-out'>
+                                <div key={index} className='font-inter capitalize font-[500] cursor-pointer text-black hover:text-[#dbdbe0] hover:bg-zinc-900 px-4 py-2'>
                                     {service}
                                 </div>
                             ))
                         }
-                        <div className='font-inter capitalize font-[500] cursor-pointer hover:text-[#6c6c71] hover:scale-105 transition-all duration-300 ease-in-out'>
+                        <div className='font-inter capitalize font-[500] cursor-pointer text-black hover:text-[#dbdbe0] hover:bg-zinc-900 px-4 py-2'>
                             Login
                         </div>
                         {<Button content={'Get Started Free'} />}
                     </motion.div>
-                )
-                </AnimatePresence>
-                
-            }
+                )}
+            </AnimatePresence>
+
+
         </nav>
     )
 }
