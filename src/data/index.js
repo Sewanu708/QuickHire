a8deebdbe2eab76ab8022ed7df3a50bbdaa72676
Sewanu Isaiah <sewanu708@gmail.com>
2025-06-14 10:39:24 +0100

    Implemented Footer

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,47 @@
 export const services = [
     'Find jobs', 'employers', 'my application', 'saved jobs'
 ]
+export const socials = [
+  {
+    id: 1,
+    name: "Facebook",
+    url: "https://facebook.com/quickhire",
+  },
+  {
+    id: 2,
+    name: "Twitter",
+    url: "https://twitter.com/quickhire",
+  },
+  {
+    id: 3,
+    name: "LinkedIn",
+    url: "https://linkedin.com/company/quickhire",
+  },
+  {
+    id: 4,
+    name: "Instagram",
+    url: "https://instagram.com/quickhire",
+  },
+];
+export const legals = [
+  {
+    id: 1,
+    title: "Privacy Policy",
+    url: "/privacy-policy",
+  },
+  {
+    id: 2,
+    title: "Terms of Service",
+    url: "/terms-of-service",
+  },
+  {
+    id: 3,
+    title: "Cookie Policy",
+    url: "/cookie-policy",
+  },
+  {
+    id: 4,
+    title: "Accessibility",
+    url: "/accessibility",
+  },
+];
