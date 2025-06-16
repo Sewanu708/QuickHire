565d50e6981c7a1ce6a2af12263127926ec5c52b
Sewanu Isaiah <sewanu708@gmail.com>
2025-06-16 08:55:36 +0100

    Carousel Implemented

--- src/Components/Brands.jsx
+++ src/Components/Brands.jsx
@@ -1,40 +1,21 @@
-import React from 'react'
-import { techBrands } from '../data'
-import { motion } from 'framer-motion';
-
-const iconVariants = {
-  animate: {
-    x: ["100%", "-100%"], 
-    transition: {
-      x: {
-        repeat: Infinity,
-        repeatType: "loop",
-        duration: 5, // adjust speed
-        ease: "linear",
-      },
-    },
-  },
-};
+import React, { useEffect, useState } from 'react';
+import { techBrands } from '../data';
+import useMeasure from 'react-use-measure';
 
 function Brands() {
     return (
-        <div className='overflow-hidden w-full bg-white py-4'>
-      <motion.div
-        className='flex gap-16 w-fit'
-        variants={iconVariants}
-        animate="animate"
-      >
-        {[...techBrands, ...techBrands].map((brand, index) => {
-          const Icon = brand.icon;
-          return (
-            <div className='text-[60px]' key={index}>
-              <Icon color={brand.color} />
+        <div className="nexcent__clients-slider">
+            <div className="nexcent__clients-track">
+                {[...techBrands, ...techBrands].map((brand, index) => (
+                    <div className="logo-item" key={index}>
+                        {<brand.icon className="text-6xl" color={brand.color} />}
+                    </div>
+                ))}
             </div>
-          );
-        })}
-      </motion.div>
-    </div>
-    )
+        </div>
+    );
 }
 
-export default Brands
+
+export default Brands;
+
